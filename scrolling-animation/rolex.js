807c0f1d4344e0935492026ef6109049abd5b8a5
Rose <10205101526@stu.ecnu.edu.cn>
2023-07-18 18:21:01 +0800

    feature:2/Animation mapped to scrolling & simple index

--- scrolling-animation/rolex.js
+++ scrolling-animation/rolex.js
@@ -18,25 +18,25 @@ const showInfoScrollOffset = 400;
 const endScrollOffset = 500;
 
 var scrollFunc = function (e) {
-  let y = window.pageYOffset;
+  let y = window.pageYOffset;//scrollTop
   if(y>startScrollOffset){
     startAnimation(y-endScrollOffset>0?endScrollOffset:y);
   }
 }
 
-window.onscroll=scrollFunc;
+window.onscroll=scrollFunc;//scroll-view bindscroll
 
 function startAnimation(y){
   let scaleValue = map(y,startScrollOffset,endScrollOffset,animationData.scale.fromValue,animationData.scale.toValue);
   let x = map(y,startScrollOffset,endScrollOffset,animationData.translateX.fromValue,animationData.translateX.toValue);
   let offset = map(y,startScrollOffset,endScrollOffset,animationData.shadowOffset.fromValue,animationData.shadowOffset.toValue);
-  let containerStyle = document.querySelector(".section2").style;
+  let containerStyle = document.querySelector(".section2").style;//selectComponent
   containerStyle.cssText = '--scale-to-value: '+ scaleValue
-  +'; --x-to-value: '+ x +'; --shadow-offset: '+ offset;
+  +'; --x-to-value: '+ x +'; --shadow-offset: '+ offset;//setStyle
   
   let bg = document.querySelector(".section3");
   if(y>showInfoScrollOffset){
-    if(!bg.classList.contains('active')){
+    if(!bg.classList.contains('active')){//hasClass
       bg.classList.add('active');
     }
   }else{
